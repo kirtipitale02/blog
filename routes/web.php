58c17aefe5f773a9861ccb57a21aab3c1c1a961c
kirtipitale02 <35273496+kirtipitale02@users.noreply.github.com>
2018-05-22 13:39:06 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -56,9 +56,13 @@ Route::delete('blog/{id}', 'BlogsController@destroy');
 Route::get('downloadExcel/{type}', 'BlogsController@downloadExcel');
 
 // cors middlewear example
+// Route::get('', ['middleware' => 'cors', function() {
+//        return view('welcome');
+// }])->middleware('auth');
+
 Route::get('', ['middleware' => 'cors', function() {
        return view('welcome');
-}])->middleware('auth');
+}]);
 
 
 
